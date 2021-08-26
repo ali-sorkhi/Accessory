66fe50a0109715c801b3a04957c73a735374cd73
ali-sorkhi <ali.sorkhi92@gmail.com>
2021-08-26 10:43:22 +0430

    tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
