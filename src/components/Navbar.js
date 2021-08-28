a42935cbc228067709c09b0d6ccc535db5a811fb
ali-sorkhi <ali.sorkhi92@gmail.com>
2021-08-28 09:43:27 +0430

    name and logo changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import logo from "../images/Logo.png";
+import logoText from "../images/LogoText.png";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
@@ -16,7 +18,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-darkgray">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -35,13 +37,13 @@ export default function Example() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    className="block lg:block h-10 w-auto"
+                    src={logo}
                     alt="Workflow"
                   />
                   <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    className="hidden lg:block h-10 w-auto"
+                    src={logoText}
                     alt="Workflow"
                   />
                 </div>
@@ -53,8 +55,8 @@ export default function Example() {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            ? "bg-lightgray text-white"
+                            : "text-lightgray hover:bg-lightgray hover:text-white",
                           "px-3 py-2 rounded-md text-sm font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      darkred: "#d90429",
+      lightred: "#ef233c",
+      white: "#edf2f4",
+      lightgray: "#8d99ae",
+      darkgray: "#2b2d42",
+    },
     extend: {},
   },
   variants: {
